92663e4bdd43b5e5b5172f9646ccf59376da8857
Tejas Ahuja <tejasahuja@Tejass-MacBook-Air.local>
2018-07-13 14:34:02 +0530

    Scroll function JQuery

---
+++ Scripts/home.js
@@ -0,0 +1,5 @@
+$(function{
+	$("a").click(function{
+	   	window.scrollTo(300, 500);
+	});
+});
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,10 +2,11 @@
  <!DOCTYPE html>
 <html>
 <head>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title> <?php echo $title." | Stuff"; ?> </title>
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
+
 
 
 </head>
