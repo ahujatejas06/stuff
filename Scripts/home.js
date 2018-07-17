70d7fc85b64950d29c423d457be2939eda9a4417
Tejas Ahuja <tejasahuja@Tejass-MacBook-Air.local>
2018-07-17 14:45:11 +0530

    Scroll Top

--- Scripts/home.js
+++ Scripts/home.js
@@ -1,24 +1,7 @@
 
 $(document).ready(function(){
-	jReady();
-	jResize();
-});
-
-$(window).resize(function(){
-	jResize();
-});
-
-function jReady()
-{
-
 	$("body, html").scrollTop(0);
-}
-function jResize()
-{
-	WinWidth = $(window).innerWidth();
-	WinHeight = $(window).innerHeight();
+});
 
 
-	// $("#home-cover").css({"height": WinHeight});
-}
 
